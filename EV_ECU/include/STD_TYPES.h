51287a628f2df393c57c1e830bfbc9bd3167d08a
Aya Ramzy <69877513+AyaRamzy@users.noreply.github.com>
2022-08-09 19:49:50 +0200

    Add files via upload

---
+++ EV_ECU/include/STD_TYPES.h
@@ -0,0 +1,17 @@
+/*********************************************************/
+/* Author      :  GP Team                                */
+/* Date        :                                         */
+/* Version     : V01                                     */
+/*********************************************************/
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char        u8 ;
+typedef unsigned short int   u16;
+typedef unsigned long  int   u32; 
+
+typedef signed char        s8 ;
+typedef signed short int   s16;
+typedef signed long  int   s32; 
+
+#endif
